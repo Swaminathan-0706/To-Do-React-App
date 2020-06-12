b7cb1f6b5a554978090565cf9848e2c48cb5d065
Swaminathan-0706 <sayeeiyer@gmail.com>
2020-06-12 22:16:05 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todos from './Components/Todos';
+import Header from './Components/Header';
 
 class App extends Component {
   constructor(props) {
@@ -39,13 +40,17 @@ class App extends Component {
   }
   //Delete Todo
   delTodo=(id)=>{
-    console.log(id);
+    this.setState({
+      todos:[...this.state.todos.filter(todo=>
+        todo.id!==id)]
+    });
   }
   render() {
       
       
       return (
           <div className="App">
+            <Header/>
              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
           </div>
       )
